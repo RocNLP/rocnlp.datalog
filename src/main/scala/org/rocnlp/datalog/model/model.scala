d22d22a80ebfaf82ada7b4857bfda748fcbc50b4
mrmechko <rbose@cs.rochester.edu>
2015-03-12 16:16:26 -0400

    "testing"

--- src/main/scala/org/rocnlp/datalog/model/model.scala
+++ src/main/scala/org/rocnlp/datalog/model/model.scala
@@ -20,36 +20,58 @@ case class Experiment(
                        parameters : Vector[Param],
                        correct : Int,
                        tagged : Int,
-                       total : Int,
-                       time : Long = System.currentTimeMillis(),
-                       valid : Boolean = true)
-
+                       total : Int
+                       )
 //Use DB as the project and Collection as the classifier
-object Experiment {
-  def apply(db : String, coll : String) =  new SalatDAO[Experiment, Int](collection = MongoConnection()(db)(coll) ) {}
+class ExperimentDAO(db : String, coll : String) {
+  var dao = new SalatDAO[Experiment, Int](collection = MongoConnection()(db)(coll) ) {}
+
+  def insert(experiment : Experiment) = {
+    //Don't insert if exact match exists
+    val exists = dao.findOne(grater[Experiment].asDBObject(experiment))
+
+    exists match {
+      case Some(_) => //do nothing since this is a duplicate
+      case None => dao.insert(experiment)
+    }
+  }
 }
 
 /**
  * Assumes the existence of a complete and good gitignore
  */
-object Versioner {
+
+case class Version(@Key("_id") id : String, valid : Boolean = true, time : Long = System.currentTimeMillis())
+
+/**
+ * Sets and gets a unique version of this code using git
+ */
+object Version {
+  val dao = new SalatDAO[Version, String](collection = MongoConnection()("meta")("versions")) {}
   import sys.process._
   def apply() : String = {
     val add = "git add ." !
     val commit = "git commit -m \"testing\"" !
     val head = "git rev-parse head" !!
 
-    head
+    dao.findOne(MongoDBObject("head" -> head)) match {
+      case Some(version) => head
+      case None => {
+        val version = Version(head)
+        dao.insert(version)
+        head
+      }
+    }
   }
 }
 
 object Test extends App {
-  val e = Experiment("test", "experiment")
+  val e = new ExperimentDAO("test", "experiment")
 
   val p1 = Param("a", 1)
   val p2 = Param("b", 2)
 
-  val exp = Experiment(Versioner(), "semcor-json", Vector(),Vector(p1, p2), 1,1,1)
+  val exp = Experiment(Version(), "semcor-json", Vector(),Vector(p1, p2), 1,1,1)
 
   e.insert(exp)
 }
