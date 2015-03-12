1e9841172e793cfdecd8c872ca1ce7f70551e7ef
mrmechko <rbose@cs.rochester.edu>
2015-03-12 13:29:07 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "datalog"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies += "org.mongodb" %% "casbah" % "2.8.0"
+
+libraryDependencies += "com.novus" %% "salat" % "1.9.9"
---
+++ src/main/scala/org/rocnlp/datalog/model/model.scala
@@ -0,0 +1,52 @@
+package org.rocnlp.datalog.model
+
+import com.novus.salat._
+import com.novus.salat.global._
+import com.novus.salat.annotations._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.MongoConnection
+
+/**
+ * Created by mechko on 3/12/15.
+ */
+
+case class Param(name : String, value : Double)
+
+case class Experiment(
+                       version : String,
+                       dataset : String,
+                       features : Vector[String],
+                       parameters : Vector[Param],
+                       correct : Int,
+                       tagged : Int,
+                       total : Int,
+                       time : Long = System.currentTimeMillis(),
+                       valid : Boolean = true)
+
+//Use DB as the project and Collection as the classifier
+object Experiment {
+  def apply(db : String, coll : String) =  new SalatDAO[Experiment, Int](collection = MongoConnection()(db)(coll) ) {}
+}
+
+/**
+ * Assumes the existence of a complete and good gitignore
+ */
+object Versioner {
+  import sys.process._
+  def apply(branch : String = "testing") : String = {
+    val checkoutBranch = "git checkout -b %s".format(branch) !
+    val addAll = ""
+  }
+}
+
+object Test extends App {
+  val e = Experiment("test", "experiment")
+
+  val p1 = Param("a", 1)
+  val p2 = Param("b", 2)
+
+  val exp = Experiment("0.01", "semcor-json", Vector(),Vector(p1, p2), 1,1,1)
+
+  e.insert(exp)
+}
